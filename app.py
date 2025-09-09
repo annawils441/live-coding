b268e309fcf4d632d659036a654f8d34d26fa1e8
annawils441 <ann4.9.4nna@gmail.com>
2025-09-09 20:18:15 +0100

    Initial commit.

--- app.py
+++ app.py
@@ -1,14 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+
+def __repr__(self):
+    return f'<Student {self.name}>'
+
+with app.app_context():
+    db.create_all()
+    
+
+@app.route('/', methods=['GET'])
 def index():
-    return '<h1>Welcome to the Homepage</h1>'
+    students= Student.query.order_by(Student.name).all()
+    return render_template('index.html', students=students)
 
-@app.route('/register')
-def register_student():
-    return 'Register Page'
+@app.route('/add_student', methods=['POST'])
+def add_student():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+
+        new_student = Student(name=name, email=email)
+
+        try:
+            db.session.add(new_student)
+            db.session.commit()
+            return redirect(url_for('index'))
+        except Exception as e:
+            return f"An error occured: {e}"
 
 if __name__ == '__main__':
     app.run(debug=True)
